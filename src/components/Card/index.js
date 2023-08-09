70e88c6cfa6b1633d1d80671089b82d65ff67437
Mateus Schulle Leite <mateusschulleleite@gmail.com>
2023-08-09 17:06:09 -0300

    biblioteca swipper

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,83 +6,9 @@ export default function Card(props) {
   console.log(tecnologiasUtilizadas);
   return (
     <div class="card">
-      <div class="card__imagem">
-        <img src={props.imagem}></img>
-      </div>
-      <div className="card__descricao">
-        <h3 className="card__descricao-titulo">{props.titulo}</h3>
-        <p className="card__descricao-texto">{props.descricao}</p>
-      </div>
-      <div className="card__links">
-        <ul className="card__links__container-links">
-          <li>
-          <img
-                width="20"
-                height="20"
-                src="https://img.icons8.com/ios/50/000/github--v1.png"
-                alt="github--v1"
-              />
-            <a href={props.repositorio}>Repo</a>
-          </li>
-          <li>
-          <img
-                width="20"
-                height="20"
-                src="https://img.icons8.com/ios/50/000/youtube-live.png"
-                alt="ive"
-              />
-            <a href={props.linklive}>Live</a>
-          </li>
-        </ul>
-        <ul className="card__links__tecnologias">
-          {tecnologiasUtilizadas.map((tecnologia, index) => (
-            <li key={index}>{tecnologia}</li>
-          ))}
-        </ul>
-      </div>
+      <img></img>
+      <h2 className="card__descricao-titulo">{props.titulo}</h2>
+      <p className="card__descricao-texto">{props.descricao}</p>
     </div>
   );
-}
-
-{
-  /*
-    <div className="card">
-      <div className="card__imagem">
-        <img src={props.imagem} alt="" />
-      </div>
-      <div className="card__descricao">
-        <div>
-          <h2>{props.titulo}</h2>
-          <div className="card__descricao__botoes">
-            <a href={props.repositorio}>
-              <img
-                width="20"
-                height="20"
-                src="https://img.icons8.com/ios/50/ffffff/github--v1.png"
-                alt="github--v1"
-              />
-              <span>Repo</span>
-            </a>
-            <a href={props.linklive}>
-              <img
-                width="20"
-                height="20"
-                src="https://img.icons8.com/ios/50/ffffff/youtube-live.png"
-                alt="ive"
-              />
-              <span>Live</span>
-            </a>
-          </div>
-        </div>
-        <p>{props.descricao}</p>
-      </div>
-      <div className="card__tecnologias">
-        <ul>
-            {tecnologiasUtilizadas.map((tecnologia, index) => 
-                <li key={index}>{tecnologia}</li>
-            )}
-        </ul>
-      </div>
-    </div>
-            */
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,40 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Portfolio.css";
 import Card from "../Card";
-import imagemCalculadora from '../../assets/calculadora.jpg';
-import imagemJogoNumeroSecreto from '../../assets/jogo-numero-secreto.jpg';
-import imagemRobotron from '../../assets/robotron.jpg';
-
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+import imagemCalculadora from "../../assets/calculadora.jpg";
+import imagemJogoNumeroSecreto from "../../assets/jogo-numero-secreto.jpg";
+import imagemRobotron from "../../assets/robotron.jpg";
 
 export default function Portfolio() {
+  useEffect(() => {
+    // Inicialize o Swiper quando o componente montar
+    new Swiper(".swiper-container", {
+      slidesPerView: 1,
+      spaceBetween: 20, // Espaçamento entre slides
+      navigation: {
+        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }, []);
   return (
     <section className="container__portfolio">
-      <h2 className="container__portfolio-titulo">Projetos</h2>
-      <div className="container__portfolio__projetos">
-        <Card
-          imagem={imagemCalculadora}
-          titulo="Calculadora"
-          descricao="Calculadora é um projeto autoral em JavaScript que oferece uma interface minimalista e funcional para realizar cálculos matemáticos de forma rápida e eficiente."
-          repositorio="https://github.com/mateusschulleleite/calculadora"
-          linklive="https://calculadora.mateusschulle.vercel.app"
-          tecnologias={["HTML", "CSS", "JavaScript"]}
-        />
-        <Card
-          imagem={imagemJogoNumeroSecreto}
-          titulo="Jogo Número Secreto"
-          descricao="Projeto criado junto ao curso de JavaScript sobre validações e reconhecimento de voz na Alura."
-          repositorio="https://github.com/mateusschulleleite/jogo-numero-secreto"
-          linklive="https://numero-secreto.mateusschulle.vercel.app"
-          tecnologias={["HTML", "CSS", "JavaScript"]}
-        />
-        <Card
-          imagem={imagemRobotron}
-          titulo="Robotron 2000"
-          descricao="Projeto criado junto ao curso de JavaScript na Alura com o objetivo de aprender a manipulação do DOm."
-          repositorio="https://github.com/mateusschulleleite/robotron-2000"
-          linklive="https://robotron.mateusschulle.vercel.app/"
-          tecnologias={["HTML", "CSS", "JavaScript"]}
-        />
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <Card
+              imagem={imagemCalculadora}
+              titulo="Calculadora"
+              descricao="Calculadora é um projeto autoral em JavaScript que oferece uma interface minimalista e funcional para realizar cálculos matemáticos de forma rápida e eficiente."
+              repositorio="https://github.com/mateusschulleleite/calculadora"
+              linklive="https://calculadora.mateusschulle.vercel.app"
+              tecnologias={["HTML", "CSS", "JavaScript"]}
+            />
+          </div>
+          <div class="swiper-slide">
+            <Card
+              imagem={imagemJogoNumeroSecreto}
+              titulo="Jogo Número Secreto"
+              descricao="Projeto criado junto ao curso de JavaScript sobre validações e reconhecimento de voz na Alura."
+              repositorio="https://github.com/mateusschulleleite/jogo-numero-secreto"
+              linklive="https://numero-secreto.mateusschulle.vercel.app"
+              tecnologias={["HTML", "CSS", "JavaScript"]}
+            />
+          </div>
+          <div class="swiper-slide">
+            <Card
+              imagem={imagemRobotron}
+              titulo="Robotron 2000"
+              descricao="Projeto criado junto ao curso de JavaScript na Alura com o objetivo de aprender a manipulação do DOm."
+              repositorio="https://github.com/mateusschulleleite/robotron-2000"
+              linklive="https://robotron.mateusschulle.vercel.app/"
+              tecnologias={["HTML", "CSS", "JavaScript"]}
+            />
+          </div>
+        </div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-scrollbar"></div>
       </div>
     </section>
   );
