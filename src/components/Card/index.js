84d9b70f27bb5680773f2a0e84a3e768e5b775f4
Mateus Schulle Leite <mateusschulleleite@gmail.com>
2023-08-10 00:06:09 -0300

    swipper

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,7 +6,7 @@ export default function Card(props) {
   console.log(tecnologiasUtilizadas);
   return (
     <div class="card">
-      <img></img>
+      <img width="50" height="50" src={props.imagem}/>
       <h2 className="card__descricao-titulo">{props.titulo}</h2>
       <p className="card__descricao-texto">{props.descricao}</p>
     </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,16 +3,18 @@ import "./Portfolio.css";
 import Card from "../Card";
 import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
-import imagemCalculadora from "../../assets/calculadora.jpg";
-import imagemJogoNumeroSecreto from "../../assets/jogo-numero-secreto.jpg";
-import imagemRobotron from "../../assets/robotron.jpg";
 
 export default function Portfolio() {
+  let telaSlide = 1;
+  if(window.screen.width >= 1024) {
+    telaSlide = 3;
+  } else if (window.screen.width >= 768) {
+    telaSlide = 2;
+  }
   useEffect(() => {
-    // Inicialize o Swiper quando o componente montar
     new Swiper(".swiper-container", {
-      slidesPerView: 1,
-      spaceBetween: 20, // Espaçamento entre slides
+      slidesPerView: telaSlide,
+      spaceBetween: 1, // Espaçamento entre slides
       navigation: {
         prevEl: ".swiper-button-prev",
         nextEl: ".swiper-button-next",
@@ -25,36 +27,31 @@ export default function Portfolio() {
   }, []);
   return (
     <section className="container__portfolio">
+      <h2 className="container__portfolio__titulo">Projetos</h2>
       <div class="swiper-container">
         <div class="swiper-wrapper">
           <div class="swiper-slide">
             <Card
-              imagem={imagemCalculadora}
+              imagem='https://img.icons8.com/ios/50/ff4900/calculator--v1.png'
               titulo="Calculadora"
               descricao="Calculadora é um projeto autoral em JavaScript que oferece uma interface minimalista e funcional para realizar cálculos matemáticos de forma rápida e eficiente."
-              repositorio="https://github.com/mateusschulleleite/calculadora"
               linklive="https://calculadora.mateusschulle.vercel.app"
-              tecnologias={["HTML", "CSS", "JavaScript"]}
-            />
+              />
           </div>
           <div class="swiper-slide">
             <Card
-              imagem={imagemJogoNumeroSecreto}
+              imagem='https://img.icons8.com/ios/50/ff4900/controller.png'
               titulo="Jogo Número Secreto"
               descricao="Projeto criado junto ao curso de JavaScript sobre validações e reconhecimento de voz na Alura."
-              repositorio="https://github.com/mateusschulleleite/jogo-numero-secreto"
               linklive="https://numero-secreto.mateusschulle.vercel.app"
-              tecnologias={["HTML", "CSS", "JavaScript"]}
             />
           </div>
           <div class="swiper-slide">
             <Card
-              imagem={imagemRobotron}
+              imagem='https://img.icons8.com/quill/50/ff4900/retro-robot.png'
               titulo="Robotron 2000"
-              descricao="Projeto criado junto ao curso de JavaScript na Alura com o objetivo de aprender a manipulação do DOm."
-              repositorio="https://github.com/mateusschulleleite/robotron-2000"
+              descricao="Projeto criado junto ao curso de JavaScript na Alura com o objetivo de aprender a manipulação do DOM."
               linklive="https://robotron.mateusschulle.vercel.app/"
-              tecnologias={["HTML", "CSS", "JavaScript"]}
             />
           </div>
         </div>
